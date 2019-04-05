27beb53ea8cc69fade65b2229d4f1aa3f0cb1ab2
tracyair <tracyair@10.7.35.128>
2019-04-04 19:34:48 -0700

    updated DirectoryStreamDemo

--- src/DirectoryStreamDemo.java
+++ src/DirectoryStreamDemo.java
@@ -1,10 +1,7 @@
 import java.io.IOException;
-
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-//import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -17,10 +14,7 @@ import java.util.ArrayList;
  * @see java.nio.file.DirectoryStream
  */
 public class DirectoryStreamDemo {
-	
-	public static ArrayList<Path> pathlist;
 
-	
 	/**
 	 * Outputs the name of the file or subdirectory, with proper indentation to help
 	 * indicate the hierarchy. If a subdirectory is encountered, will recursively
@@ -35,34 +29,21 @@ public class DirectoryStreamDemo {
 	 *               passed
 	 * @throws IOException
 	 */
-
-	private static void privatetxttraverse(Path path) throws IOException {
-		/*
-		 * The try-with-resources block makes sure we close the directory stream when
-		 * done, to make sure there aren't any issues later when accessing this
-		 * directory.
-		 *
-		 * Note, however, we are still not catching any exceptions. This type of try
-		 * block does not have to be accompanied with a catch block. (You should,
-		 * however, do something about the exception.)
-		 */
-		try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)) {
-			// Efficiently iterate through the files and subdirectories.
-			for (Path file : listing) {
-				// Check if this is a subdirectory
-				if (Files.isDirectory(file)) {
-					// Recursively traverse the subdirectory.
-					privatetxttraverse(file);
-				}
-				else {
-					// Add the file size next to the name
-					if(file.getFileName().toString().toLowerCase().endsWith(".txt") || file.getFileName().toString().toLowerCase().endsWith(".text")) {
-						pathlist.add(file);
-					}
-					
+	// traverse the text files
+	private static void privateTraverse(Path path, ArrayList<Path> paths) throws IOException {
+		if (Files.isDirectory(path)) {
+			try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)) {
+				for (Path file : listing) {
+					DirectoryStreamDemo.privateTraverse(file, paths);
 				}
 			}
+		} else {
+			if (path.getFileName().toString().toLowerCase().endsWith(".txt")
+					|| path.getFileName().toString().toLowerCase().endsWith(".text")) {
+				paths.add(path);
+			}
 		}
+
 	}
 
 	/**
@@ -70,65 +51,13 @@ public class DirectoryStreamDemo {
 	 * class can access this method, so some validation is required.
 	 *
 	 * @param directory to traverse
+	 * @return an ArrayList of text files
 	 * @throws IOException
 	 */
-	public static void publictxttraverse(Path directory) throws IOException {
-		pathlist = new ArrayList<>();
-		if (Files.isDirectory(directory)) {
-			privatetxttraverse(directory);
-		}
-		else {
-			if(directory.getFileName().toString().toLowerCase().endsWith(".txt") || directory.getFileName().toString().toLowerCase().endsWith(".text")) {
-				pathlist.add(directory);
-			}
-		}
-	}
-	
-	
-	private static void privatehtmltraverse(Path path) throws IOException{
-		try (DirectoryStream<Path> listing = Files.newDirectoryStream(path)) {
-			for (Path file : listing) {
-				if (Files.isDirectory(file)) {
-					privatehtmltraverse(file);
-				}
-				else {
-					if(file.toString().toLowerCase().endsWith(".html")) {
-						pathlist.add(file);
-					}
-
-				}
-			}
-		}
-		
-	}
-	
-	
-	public static void publichtmltraverse(Path directory) throws IOException {
-		pathlist = new ArrayList<>();
-		if (Files.isDirectory(directory)) {
-			privatehtmltraverse(directory);
-		}
-		else {
-			if(directory.toString().toLowerCase().endsWith(".html")) {
-				pathlist.add(directory);
-			}
-		}
-	}
-	
-	
-
-	/**
-	 * Recursively traverses the current directory and prints the file listing.
-	 *
-	 * @param args unused
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {		
-		Path path = Paths.get(".").toAbsolutePath().normalize();
-		System.out.println(path.getFileName() + ":");
-		
-		publictxttraverse(path);
-		publichtmltraverse(path);
+	public static ArrayList<Path> publicTraverse(Path directory) throws IOException {
+		ArrayList<Path> paths = new ArrayList<>();
+		privateTraverse(directory, paths);
+		return paths;
 	}
 
 }
