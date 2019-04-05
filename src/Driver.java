af23fcbbc992db137de3f15814988526a3205f56
tracyair <tracyair@10.7.35.128>
2019-04-04 17:37:24 -0700

    add QueryParser

--- src/Driver.java
+++ src/Driver.java
@@ -22,77 +22,75 @@ public class Driver {
 	 * inverted index.
 	 *
 	 * @param args flag/value pairs used to start this program
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	
+
 	public static void main(String[] args) throws IOException {
-		
-				// store initial start time
-				Instant start = Instant.now();
-				
-				// TODO Modify this method as necessary.				
-				Path path, index;
-				ArgumentMap argmap = new ArgumentMap(args);
-				WordIndex invertedindex = new WordIndex();
-				if(argmap.hasFlag("-path")) {
-					path = argmap.getPath("-path");
-					//check valid input path
-					if (path != null) {
-						//check if it has "-index".
-						if(argmap.hasFlag("-index")) {
-							
-							//traverse to all text file.
-							DirectoryStreamDemo.publictxttraverse(path);
-							// get output path, if no output path, output to the default path "index.json".
-							index = argmap.getPath("-index", Paths.get("index.json"));
-						}
-						//if no "-index", traverse all html file.
-						else {
-							DirectoryStreamDemo.publictxttraverse(path);
-							index = null;
-						}				
-						 //traverse all path in the pathlist using DirectoryStreamDemo
-						for (Path file : DirectoryStreamDemo.pathlist) {
-							// read the file and parse the file word by word
-							ArrayList<String> stemlist = TextFileStemmer.stemFile(file);
 
-							//add each word to the invertedindex
-							int position = 1;
+		// store initial start time
+		Instant start = Instant.now();
 
-							for (String stem : stemlist) {
+		// TODO Modify this method as necessary.
+		Path path, index;
+		ArgumentMap argmap = new ArgumentMap(args);
+		WordIndex invertedindex = new WordIndex();
+		if (argmap.hasFlag("-path")) {
+			path = argmap.getPath("-path");
+			// check valid input path
+			if (path != null) {
+				// check if it has "-index".
+				if (argmap.hasFlag("-index")) {
 
-								invertedindex.add(stem, file, position);
-								position++;
-							}
-
-						}		
-						//if it is txt file, convert it to json format and print the locations/wordcount.
-						if(index!=null) {
-							
-							PrettyJSONWriter.asNestedTreeMapMap(invertedindex.getDictionary(), index);
-						}
-						
-						//check if has "-locations", and output the wordcount of each txt file.
-						if(argmap.hasFlag("-locations")) {
-							//get the output path, or output to default path "loca5tions.json".
-							Path locations = argmap.getPath("-locations", Paths.get("locations.json"));
-							invertedindex.count(path);
-							PrettyJSONWriter.asObject(invertedindex.getLocationsMap(), locations);
-						}
-					}	
+					// traverse to all text file.
+					DirectoryStreamDemo.publictxttraverse(path);
+					// get output path, if no output path, output to the default path "index.json".
+					index = argmap.getPath("-index", Paths.get("index.json"));
 				}
+				// if no "-index", traverse all html file.
 				else {
-					path = null;
-					index = Paths.get("index.json");
-					PrettyJSONWriter.asNestedTreeMapMap(invertedindex.getDictionary(), index);	
+					DirectoryStreamDemo.publictxttraverse(path);
+					index = null;
 				}
-				// calculate time elapsed and output
-				Duration elapsed = Duration.between(start, Instant.now());
-				double seconds = (double) elapsed.toMillis() / Duration.ofSeconds(1).toMillis();
-				System.out.printf("Elapsed: %f seconds%n", seconds);
+				// traverse all path in the pathlist using DirectoryStreamDemo
+				for (Path file : DirectoryStreamDemo.pathlist) {
+					// read the file and parse the file word by word
+					ArrayList<String> stemlist = TextFileStemmer.stemFile(file);
+
+					// add each word to the invertedindex
+					int position = 1;
+
+					for (String stem : stemlist) {
+
+						invertedindex.add(stem, file, position);
+						position++;
+					}
+
+				}
+				// if it is txt file, convert it to json format and print the
+				// locations/wordcount.
+				if (index != null) {
+
+					PrettyJSONWriter.asNestedTreeMapMap(invertedindex.getDictionary(), index);
+				}
+
+				// check if has "-locations", and output the wordcount of each txt file.
+				if (argmap.hasFlag("-locations")) {
+					// get the output path, or output to default path "loca5tions.json".
+					Path locations = argmap.getPath("-locations", Paths.get("locations.json"));
+					invertedindex.count(path);
+					PrettyJSONWriter.asObject(invertedindex.getLocationsMap(), locations);
+				}
+			}
+		} else {
+			path = null;
+			index = Paths.get("index.json");
+			PrettyJSONWriter.asNestedTreeMapMap(invertedindex.getDictionary(), index);
+		}
+		// calculate time elapsed and output
+		Duration elapsed = Duration.between(start, Instant.now());
+		double seconds = (double) elapsed.toMillis() / Duration.ofSeconds(1).toMillis();
+		System.out.printf("Elapsed: %f seconds%n", seconds);
 	}
-		
-		
 
 	/*
 	 * Generally, "driver" classes are responsible for setting up and calling other
---
+++ src/QueryParser.java
@@ -0,0 +1,7 @@
+/**
+ * @author tracyair
+ *
+ */
+public class QueryParser {
+
+}
