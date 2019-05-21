1d05513e183a85e605882a007b1ffbf6f1014d30
tracyair <tracyair@10.0.0.217>
2019-05-21 14:17:17 -0700

    make changes

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.HashSet;
+
+import opennlp.tools.stemmer.Stemmer;
+import opennlp.tools.stemmer.snowball.SnowballStemmer;
 
 /**
  * @author tracyair
@@ -24,6 +28,49 @@ public class WebCrawler {
 		this.index = index;
 		this.queue = new WorkQueue(threads);
 		this.threads = threads;
+	}
+
+	public void crawl(URL seed, int limit) {
+		links.add(seed);
+		queue.execute(new Task(seed, limit));
+		queue.finish();
+		queue.shutdown();
+	}
+
+	private class Task implements Runnable {
+		private final URL singleURL;
+		private final int limit;
+
+		/**
+		 * @param url
+		 * @param limit
+		 */
+		public Task(URL url, int limit) {
+
+			this.singleURL = url;
+			this.limit = limit;
+		}
+
+		@Override
+		public void run() {
+			try {
+				var HTML = HtmlFetcher.fetchHTML(this.singleURL, 3);
+				if (HTML == null) {
+					return;
+				}
+				InvertedIndex local = new InvertedIndex();
+				Stemmer stemmer = new SnowballStemmer(SnowballStemmer.ALGORITHM.ENGLISH);
+				int start = 1;
+				for (String s : TextParser.parse(HtmlCleaner.stripHtml(HTML))) {
+					local.add(stemmer.stem(s).toString(), this.singleURL.toString(), start);
+					start++;
+				}
+			} catch (IOException e) {
+				System.out.println("Unable to crawl with the url provided");
+			}
+
+		}
+
 	}
 
 }
